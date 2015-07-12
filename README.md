9217adb632e6e69299318d0fbbcde1218ebbb04d
Glynn Bird <glynn.bird@gmail.com>
2015-07-12 08:15:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Node.js Starter Application
+
+Bluemix provides a Node.js starter application as a template so that you can
+add your code and push the changes back to the Bluemix environment.
+
+## Files
+
+The Node.js starter application has files as below:
+
+* app.js
+
+	This file contains the server side JavaScript code for your application
+	written using the express server package.
+
+* public/
+
+	This directory contains public resources of the application, that will be
+	served up by this server
+
+* package.json
+
+	This file contains metadata about your application, that is used by both
+	the `npm` program to install packages, but also Bluemix when it's
+	staging your application.  For more information, see:
+	<https://docs.npmjs.com/files/package.json>
---
+++ app.js
@@ -0,0 +1,37 @@
+/*jshint node:true*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as it's web server
+// for more info, see: http://expressjs.com
+var express = require('express'),
+  cors = require('cors'),
+  cfenv = require('cfenv'),
+  appEnv = cfenv.getAppEnv(),
+  app = express(),
+  cloudant = require('./lib/db.js');
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+
+// search proxy
+app.get('/search', cors(), function (req, res) {
+  cloudant.search(req.query, function(err, data) {
+    if (err) {
+      return res.status(err.statusCode).send({error: err.error, reason: err.reason});
+    }
+    res.send(data);
+  });
+});
+
+
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, appEnv.bind, function() {
+
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
